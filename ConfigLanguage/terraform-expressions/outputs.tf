101c2acfd9f17ef87276907060c0a46611b251ae
Jodionisio Muachifi <jmuachifi@gmail.com>
2021-10-05 10:39:11 +0100

    Terraform Expressions

---
+++ ConfigLanguage/terraform-expressions/outputs.tf
@@ -0,0 +1,8 @@
+#output "tags" {
+output "first_tags" {
+  #value = aws_instance.ubuntu.tags
+  value = aws_instance.ubuntu[0].tags
+}
+output "private_addresses" {
+  value = aws_instance.ubuntu[*].private_dns
+}
---
+++ ConfigLanguage/terraform-expressions/variables.tf
@@ -0,0 +1,34 @@
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "172.16.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "172.16.10.0/24"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy your instance"
+  default     = "us-east-2"
+}
+
+variable "name" {
+  description = "The username assigned to the infrastructure"
+  default     = "terraform"
+}
+
+variable "team" {
+  description = "The team responsible for the infrastructure"
+  default     = "hashicorp"
+}
+variable "owner" {
+  description = "The owner responsible for all"
+  default     = "hashicorpOwner"
+}
+variable "high_availability" {
+  type        = bool
+  description = "If this is a multiple instance deployment, choose `true` to deploy 3 instances"
+  default     = true
+}
+
+
